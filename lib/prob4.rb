07ac66f75cdab9d7bbeca9484527f754a07cc8c7
Saikat Bhadra <saikat.bhadra@gmail.com>
2015-12-23 22:52:53 -0800

    Finish problem 4

--- lib/prob4.rb
+++ lib/prob4.rb
@@ -4,6 +4,16 @@
 # The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
 # Find the largest palindrome made from the product of two 3-digit numbers.
 
+def largest_palidrome
+  number_set = (999).downto(100).to_a
+  number_products = number_set.combination(2).map { |n| n.inject(:*) }.sort.reverse.uniq
+
+  number_products.each do |number|
+    return number if palindrome?(number)
+  end
+
+  nil
+end
 
 def palindrome?(number)
   number_string = number.to_s
@@ -11,5 +21,5 @@ def palindrome?(number)
 end
 
 if __FILE__==$0
-  p palindrome?(11)
+  p largest_palidrome
 end
